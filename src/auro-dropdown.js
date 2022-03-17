55c79ecf7925a3635ae11ab91d18957e4b72b5b1
Capsule42 <jason@capsule42.com>
2022-03-17 10:38:12 -0700

    fix(toggle): prevent default key behavior when toggle on dropdown

--- src/auro-dropdown.js
+++ src/auro-dropdown.js
@@ -146,6 +146,7 @@ class AuroDropdown extends LitElement {
       const key = event.key.toLowerCase();
 
       if (key === ' ' || key === 'enter') {
+        event.preventDefault();
         toggleDropdown; /* eslint-disable-line no-unused-expressions */
       }
     };
