6924d04b37f41d6ed4abb6cb3a822ffd1e82d71c
Dale Sande <dale.sande@gmail.com>
2024-01-18 19:50:45 -0800

    feat: add suport for SSR projects #213
    This commit updates scripts and configs to support SSR style projects.
    
    Changes to be committed:
    renamed:    commitlint.config.js -> commitlint.config.cjs
    modified:   package-lock.json
    modified:   package.json
    modified:   packageScripts/postinstall.mjs
    renamed:    scripts/generateDocs.js -> scripts/generateDocs.mjs
    renamed:    scripts/postCss.js -> scripts/postCss.mjs
    modified:   scripts/pre-commit.mjs

--- scripts/generateDocs.mjs
+++ scripts/generateDocs.mjs
@@ -1,8 +1,9 @@
-const path = require('path');
-const chalk = require('chalk');
-const markdownMagic = require('markdown-magic');
-const fs = require('fs');
-const https = require('https');
+import path from 'path';
+import markdownMagic from 'markdown-magic';
+import fs from 'fs';
+import https from 'https';
+
+const __dirname = new URL('.', import.meta.url).pathname;
 
 const readmeTemplateUrl = 'https://raw.githubusercontent.com/AlaskaAirlines/WC-Generator/master/componentDocs/README.md';
 const dirDocTemplates = './docTemplates';
@@ -12,29 +13,29 @@ const readmeFilePath = dirDocTemplates + '/README.md';
  * Extract NPM, NAMESPACE and NAME from package.json
  */
 
- function nameExtraction() {
-  const packageJson = fs.readFileSync('package.json', 'utf8', function(err, data) {
-    if (err) {
-      console.log(chalk.red('ERROR: Unable to read package.json file', err));
-    }
-  })
+function nameExtraction() {
+ const packageJson = fs.readFileSync('package.json', 'utf8', function(err, data) {
+   if (err) {
+     console.log('ERROR: Unable to read package.json file', err);
+   }
+ })
 
-  pName = JSON.parse(packageJson).name;
+ let pName = JSON.parse(packageJson).name;
 
-  let npmStart = pName.indexOf('@');
-  let namespaceStart = pName.indexOf('/');
-  let nameStart = pName.indexOf('-');
+ let npmStart = pName.indexOf('@');
+ let namespaceStart = pName.indexOf('/');
+ let nameStart = pName.indexOf('-');
 
-  let result = {
-    'npm': pName.substring(npmStart, namespaceStart),
-    'namespace': pName.substring(namespaceStart + 1, nameStart),
-    'namespaceCap': pName.substring(namespaceStart + 1)[0].toUpperCase() + pName.substring(namespaceStart + 2, nameStart),
-    'name': pName.substring(nameStart + 1),
-    'nameCap': pName.substring(nameStart + 1)[0].toUpperCase() + pName.substring(nameStart + 2)
-  };
+ let result = {
+   'npm': pName.substring(npmStart, namespaceStart),
+   'namespace': pName.substring(namespaceStart + 1, nameStart),
+   'namespaceCap': pName.substring(namespaceStart + 1)[0].toUpperCase() + pName.substring(namespaceStart + 2, nameStart),
+   'name': pName.substring(nameStart + 1),
+   'nameCap': pName.substring(nameStart + 1)[0].toUpperCase() + pName.substring(nameStart + 2)
+ };
 
-  return result;
-};
+ return result;
+}
 
 /**
  * Replace all instances of [npm], [name], [Name], [namespace] and [Namespace] accordingly
@@ -48,16 +49,10 @@ function formatTemplateFileContents(content, destination) {
    * Replace placeholder strings
    */
   result = result.replace(/\[npm]/g, nameExtractionData.npm);
-  result = result.replace(/\[name]/g, nameExtractionData.name);
-  result = result.replace(/\[Name]/g, nameExtractionData.nameCap);
+  result = result.replace(/\[name](?!\()/g, nameExtractionData.name);
+  result = result.replace(/\[Name](?!\()/g, nameExtractionData.nameCap);
   result = result.replace(/\[namespace]/g, nameExtractionData.namespace);
   result = result.replace(/\[Namespace]/g, nameExtractionData.namespaceCap);
-
-  /**
-   * Strip all markdown-magic comments
-   */
-  result = result.replace(/<!-- AURO(.*?)-GENERATED-CONTENT(.*?)-->/g, '');
-  result = result.replace(/<!-- The below (.*?) is automatically added from(.*?)-->/g, '');
 
   /**
    * Cleanup line breaks
@@ -72,7 +67,7 @@ function formatTemplateFileContents(content, destination) {
    * Write the result to the destination file
    */
   fs.writeFileSync(destination, result, { encoding: 'utf8'});
-};
+}
 
 function formatApiTableContents(content, destination) {
   const nameExtractionData = nameExtraction();
@@ -81,7 +76,7 @@ function formatApiTableContents(content, destination) {
   let result = content;
 
   result = result
-    .replace(/\r\n|\r|\n####\s([a-zA-Z]*)/g, `\r\n#### <a name="$1"></a>$1`)
+    .replace(/\r\n|\r|\n####\s`([a-zA-Z]*)`/g, `\r\n#### <a name="$1"></a>\`$1\`<a href="#" style="float: right; font-size: 1rem; font-weight: 100;">back to top</a>`)
     .replace(/\r\n|\r|\n\|\s`([a-zA-Z]*)`/g, '\r\n| [$1](#$1)')
     .replace(/\| \[\]\(#\)/g, "");
 
@@ -93,41 +88,18 @@ function formatApiTableContents(content, destination) {
 }
 
 /**
- * If auroLabs project, include auroLabs documentation in `./README.md`
- */
-
-function processLabsReadmeContent() {
-  let nameExtractionData = nameExtraction();
-
-  if (nameExtractionData.npm === '@aurolabs') {
-    const callbackAurolabs = function(updatedContent, outputConfig) {
-      console.log(chalk.green('Readme updated to reference AuroLabs content.'));
-    };
-
-    const configAurolabs = {
-      matchWord: 'AUROLABS-GENERATED-CONTENT'
-    };
-
-    const markdownPathAurolabs = path.join(__dirname, '../README.md');
-
-    markdownMagic(markdownPathAurolabs, configAurolabs, callbackAurolabs);
-  }
-}
-
-/**
  * Compiles `./docTemplates/README.md` -> `./README.md`
  */
 
 function processReadme() {
   const callback = function(updatedContent, outputConfig) {
-    processLabsReadmeContent()
 
     if (fs.existsSync('./README.md')) {
       fs.readFile('./README.md', 'utf8', function(err, data) {
         formatTemplateFileContents(data, './README.md');
       });
     } else {
-      console.log(chalk.red('ERROR: ./README.md file is missing'));
+      console.log('ERROR: ./README.md file is missing');
     }
   };
 
@@ -139,8 +111,6 @@ function processReadme() {
   const markdownPath = path.join(__dirname, '../docTemplates/README.md');
 
   markdownMagic(markdownPath, config, callback);
-
-  processLabsReadmeContent();
 }
 
 /**
@@ -154,7 +124,7 @@ function processDemo() {
         formatTemplateFileContents(data, './demo/demo.md');
       });
     } else {
-      console.log(chalk.red('ERROR: ./demo/demo.md file is missing'));
+      console.log('ERROR: ./demo/demo.md file is missing');
     }
   };
 
@@ -179,7 +149,7 @@ function processApiExamples() {
         formatApiTableContents(data, './demo/apiExamples.md');
       });
     } else {
-      console.log(chalk.red('ERROR: ./demo/apiExamples.md file is missing'));
+      console.log('ERROR: ./demo/apiExamples.md file is missing');
     }
   };
 
@@ -206,7 +176,7 @@ function copyReadmeLocally() {
   if (!fs.existsSync(readmeFilePath)) {
     fs.writeFile(readmeFilePath, '', function(err) {
       if(err) {
-        console.log(chalk.red('ERROR: Unable to create README.md file.', err));
+        console.log('ERROR: Unable to create README.md file.', err);
       }
     });
   }
@@ -219,7 +189,7 @@ function copyReadmeLocally() {
     });
 
   }).on('error', (err) => {
-    console.log(chalk.red('ERROR: Unable to fetch README.md file from server.', err));
+    console.log('ERROR: Unable to fetch README.md file from server.', err);
   });
 }
 
